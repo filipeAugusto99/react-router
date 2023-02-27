b1624113536d6f7cce570aff174d6bbf5e17dc53
Filipe Augusto <filipecordeiro213@gmail.com>
2023-02-27 08:38:37 -0300

    react router-defining routes

---
+++ src/pages/Users.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Users() {
+  return (
+    <>
+    <h1>Usuários</h1>
+    <Link to="/">home</Link>
+    </>
+  )
+}
+
+export default Users
